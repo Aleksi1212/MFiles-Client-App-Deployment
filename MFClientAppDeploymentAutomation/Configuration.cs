f6f54d283e5f15030e517c6de482a6316fe7e02c
Aleksi Noro <aleksinoro@gmail.com>
2024-01-23 10:04:38 +0200

    add test case to CurrentDirectory, and seperate AppConfig and VaultConfig

--- MFClientAppDeploymentAutomation/Configuration.cs
+++ MFClientAppDeploymentAutomation/Configuration.cs
@@ -9,9 +9,19 @@ using System.Threading.Tasks;
 
 namespace MFClientAppDeploymentAutomation
 {
-    internal class Configuration
+    internal class AppConfiguration
     {
-        public DirectoryInfo CurrentDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
+        private readonly bool testEnvironment = new Utils().IsTest();
+
+        public DirectoryInfo CurrentDirectory
+        {
+            get
+            {
+                return testEnvironment
+                    ? new DirectoryInfo("C:\\Users\\A505471\\source\\repos\\MFClientAppDeploymentAutomation\\MFClientAppDeploymentAutomation\\test\\")
+                    : new DirectoryInfo(Directory.GetCurrentDirectory());
+            }
+        }
         public string AppFilePath
         {
             get { return Path.Combine(Environment.GetEnvironmentVariable("CLIENT_APP_BUILD_FOLDER"), $"{CurrentDirectory.Name}.zip"); }
@@ -20,8 +30,6 @@ namespace MFClientAppDeploymentAutomation
         {
             get
             {
-                bool testEnvironment = new Utils().IsTest();
-
                 string appdefPath = testEnvironment
                     ? "C:\\Users\\A505471\\source\\repos\\MFClientAppDeploymentAutomation\\MFClientAppDeploymentAutomation\\test\\appdef.xml"
                     : Path.Combine(CurrentDirectory.FullName, "appdef.xml");
@@ -32,17 +40,20 @@ namespace MFClientAppDeploymentAutomation
                 return xmlDoc.SelectSingleNode("//guid")?.InnerText;
             }
         }
-        public string VaultName = "Aleksin hiekkalaatikka (VAN02)";
-        
-        public MFAuthType AuthType = MFAuthType.MFAuthTypeSpecificWindowsUser;
-        public string UserName = "mfilestest";
-        public string Password = Environment.GetEnvironmentVariable("VAN02PASSWORD");
-        public string Dmain = "dekrafinland.fi";
-        public string Spn = "";
-        public string ProtocolSequence = "ncacn_ip_tcp";
-        public string NetworkAddress = "van02.dekra.fi";
-        public int Endpoint = 2266;
-        public bool EncryptedConnection = false;
-        public string LocalComputerName = "";
+    }
+
+    internal class VaultConfiguration
+    {
+        public readonly string VaultName = "Aleksin hiekkalaatikka (VAN02)";
+        public readonly MFAuthType AuthType = MFAuthType.MFAuthTypeSpecificWindowsUser;
+        public readonly string UserName = "mfilestest";
+        public readonly string Password = Environment.GetEnvironmentVariable("VAN02PASSWORD");
+        public readonly string Domain = "dekrafinland.fi";
+        public readonly string Spn = "";
+        public readonly string ProtocolSequence = "ncacn_ip_tcp";
+        public readonly string NetworkAddress = "van02.dekra.fi";
+        public readonly string Endpoint = "2266";
+        public readonly bool EncryptedConnection = false;
+        public readonly string LocalComputerName = "";
     }
 }
